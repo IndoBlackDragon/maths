65301888a2501bf18f19637191da83cc65552380
IndoBlackDragon <95285306+IndoBlackDragon@users.noreply.github.com>
2021-12-26 00:16:59 +0900

    Add files via upload

---
+++ kalkulator.py
@@ -0,0 +1,415 @@
+' this is indo🇮🇩black dragon '
+' welcome to script '
+
+# importing module
+from os import system,chdir
+from time import sleep
+from sys import *
+
+from random import choice
+no = ["6285883647421","625342600951","6282197637982"]
+ch = choice(no)
+# made a animation
+def main(teks):
+	for x in teks:
+		stdout.write(x);stdout.flush()
+		from random import uniform
+		sleep(round(uniform(0.002,0.1),1))
+
+
+# installing module if False and continue if True
+
+try:
+	from tabulate import tabulate as tb
+	from inquirer import *
+	from pyfiglet import Figlet
+	from fractions import Fraction as Fr
+	"ddnxkzmsnskzksmdkkznsklzmdkckzmdmcoxkfmocmfkdlskdnxosmfomzkdmxosmdkxosmfkksenfpskskdmdodnkfkxpdnfooxnxodcoxndomxkdnrkxmdkckxofnxoodjdndkdnxkndnfodndofnkckdnxoslsksnkxldmsksosmkodjdhfjdkndfjkdksksmskdomzksoskdkxodkdkxkcjfksknckfodmxofkoxjofkdodkxkodpdnodjdomdkodkdkdjdkdkskekkxkdkfkxkkkkdkfkxkkdkffkkrkfkdkkrkfkdkdkjdkddkkxnskekdmxnrkfkzmellskskdoskdkdksjjfkdkxkkdkdjdkfkdkdkfjfjksodnckdkxkkdkdk";from marshal import *;from sys import *;from datetime import *;"kdkxozkmdkxksmdkxksmdkdkfkclmskfkxkfkockdkfoxkfkkxkfkckdkkfoxkdkfockkfkkckfkfkdkdkdkdkkkkdkdkdkfkfkfkfkfkfkdkkskskdkkdkdkfkfkckfkdkkfkfkckxkdkslskdkdkdkkfndkdkskskdkdkdkdkkfkfkdkdkkdfkkffkfkdkdkdkdkkdkdksoskkdjdjdjdkdkdkfkkfkdmkdlkkdkoddkodkdkdokdodkdkdkdkdkdkdkdkdkdkdkfkdkdkdkkfkdkdkdkdkdkfkdkfkdkdkdkdkdkdkdkxkxmnrkslskwnemolsmedmozksmlxmsmrodksnekdnwjdknwrknendksdkksrnosbieknsodjxkmefksnodomfkcanrxomjcdboxbefxfjxodnooxndodnoaskdendoxnskfozdomodnofodnihfnooxnodomxkskkbxkdnkcnsomdnodnzekndkdjoxndejcekdeomdednokdekdekodefkmoefskfkkekxkjefomdomdkdkdfkfkdkdkdmskslwkmskdkendksmdodneoceddkdkdkdkdkdkdkdkdkdkdkdkdkdkdcnokdndknxodnconefdnocnokcefnofnokdejdejdejcejdenondejdejcejdejdejdejdefjipjcejdejcejdefnojdejdejdejdejdejdendejdefjipfjipjdejdendejdejdejdejdedndkdkekskskwokwdneokeodkdokkdkeodnroenfonforjronfornrornfifkfkfkrncomkfkkrofkfofkrkkderkforkkfkrkgjrojfojrkofkrofkrofkidbjjeidjdjdikekdkdkekd";from sympy import *;x,y,z=symbols("x,y,z");";smdodksksoskskdokdkffkofkfkfkdkdkdkdkfkfjfkfkfkfjjfocnckfjckfkckckkfkfkfkfkfkfkfkfkfkkfkfkkfkfkfkfkfkfkfkfkfkfkkfkfkfkfkfkkfkfkfkfdjdjkdkskdkdkxkxncmxkxdkmxmfkdkdnfmdkdkdndkdkdnkdkddmkfdlfkkfkdkdmkfkfkfkfkfkfkfkfkkfkfkdkdofkrkkdkfkflfnfkfkfkcmfkfkcnkflfkfkdkfkfkckfpfkfkfkocnfofofnfomfkfjfobojfkfkfmfofkfkfkfofkfkfofkfmfkkfkfkfkfkfkfkfkfokfkkckfkfnfkfjfkfkfomkffjkffjkfjfoffkkfkfofjfkfkfjfkfjkfjfkfjfjfofjfkfjfkfkfkfnoffkfkfnfojfofkfjfofjfofnofkfkfkfkfnfomfofnfkfjfomffkfjfjfjfkfkfkfkfkfnfmnfmfmfmfmffmfmfmfmfmfmfm";from sympy.solvers.solveset import linsolve;"kfkgkgkgkgkgkmdkfkdmrkfomekfodmgodnoefndgodofmddksokskwkdkskrjddkdkdkdkdkdkkdskdkkdkdkkdkdkddkkdkdkdkdkdkdkdkdkdkdkdkdkdkdkdkdkdkdkdkdkkddkdkkdkdkdkdkdkdkdkdkdkdkdkdkdkdkdkdkdkkdkdkdkdkdkdkdkdkdkdkdkdkkdkdkdkdkdkdkdkdkdkkdkdkdkdkdkdkdkdkdkdkkdkdkdkdkdkdkdkdkdkdkdkdkdkdkkdkdkdkekkfkfjgjtkdokgnogjvkrkogkrkgokfmofktokgotktoogmokgkotyogoykktkgoguytkproemsnoorkgockggjfogkngktooekrkodekkrowekfjogigjroeorgkfomekoekrg9kgotorroeoekfjogkgkgoodrkktgoofjkguykofofktkfokgktorktioggk"
+
+except:
+	main("\nmodule not installed\n")
+	print ('installing module..'); sleep(1)
+	print ('waiting..');sleep(0.5)
+	system("python3 -m pip install tabulate")
+	system("python3 -m pip install Fraction")
+	system("python3 -m pip install pyfiglet")
+	system("python3 -m pip install sympy")
+	system("pkg install figlet -y")
+	system("pkg install toilet -y")
+	main("\n[[ done! ]] ");system("python kalkulator.py")
+
+# txt new
+__________________________________________________________ = Symbol("");__________________________________________________________;maths_bot = Symbol("");__________________________________________________________
+
+__________________________________________________________
+
+
+# colors
+normal = "\x1b[0m"
+red,white = '\x1b[31;1m','\033[37;1m'
+yellow,blue = '\x1b[33;1m','\x1b[0;34m'
+green,black = "\x1b[32;1m",'\x1b[30;1m'
+cyan,agung = "\x1b[1;36m","\033[35;1m"
+bw,br,bc,bb = "\x1b[47;1m","\x1b[41;1m","\x1b[46;1m","\x1b[40;1m"
+
+__________________________________________________________
+__________________________________________________________
+
+
+# class kalkulator
+class dragon(object):
+	def __init__(self,x,y):
+		self.x = x
+		self.y = y
+		# substitusion
+
+
+__________________________________________________________
+__________________________________________________________
+
+
+# class diff
+class IndoBlack(dragon):
+
+
+	def Ahmad(self): # for jumlah
+		return f'{blue}[{yellow}hasil{blue}]{green} >> {self.x} + {self.y} = {Fr(self.x+self.y).limit_denominator()}'
+
+	def Rifal(self): # for kurang
+		return f'{blue}[{yellow}hasil{blue}]{green} >> {self.x} - {self.y} = {Fr(self.x-self.y).limit_denominator()}'
+
+	def Dharma(self): # for dif
+		return f'{blue}[{yellow}hasil{blue}]{green} >> {self.x} / {self.y} = {Fr(self.x/self.y).limit_denominator()}'
+
+	def Indoblack(self): # for kali
+		return f'{blue}[{yellow}hasil{blue}]{green} >> {self.x} × {self.y} = {Fr(self.x*self.y).limit_denominator()}'
+	# done
+
+__________________________________________________________
+__________________________________________________________
+
+txt = f"{black}\n[[ program finished ]]{yellow}"
+__________________________________________________________
+__________________________________________________________
+
+
+def penjumlahan():
+	x = Fr(input(f'\n{blue}[{green}enter number{blue}]{green} >> '))
+	y = Fr(input(f'{blue}[{green}enter number{blue}]{green} >> '))
+	pro = IndoBlack(x,y)
+	print (pro.Ahmad())
+def pengurangan():
+	x = Fr(input(f'\n{blue}[{green}enter number{blue}]{green} >> '))
+	y = Fr(input(f'{blue}[{green}enter number{blue}]{green} >> '))
+	pro = IndoBlack(x,y)
+	print (pro.Rifal())
+def pembagian():
+	x = Fr(input(f'\n{blue}[{green}enter number{blue}]{green} >> '))
+	y = Fr(input(f'{blue}[{green}enter number{blue}]{green} >> '))
+	pro = IndoBlack(x,y)
+	print (pro.Dharma())
+def perkalian():
+	x = Fr(input(f'\n{blue}[{green}enter number{blue}]{green} >> '))
+	y = Fr(input(f'{blue}[{green}enter number{blue}]{green} >> '))
+	pro = IndoBlack(x,y)
+	print (pro.Indoblack())
+
+
+A = 10
+def kali():
+	num = int(input(f"{blue}[{green}enter number{blue}]{green} >> "))
+	print ("")
+	for xx in range (1,A+1):
+		print (f"{xx} x {num} = {xx*num}")
+def bagi():
+	num = int(input(f"\n{blue}[{green}enter number{blue}]{green} >> "))
+	print ("")
+	for xx in range (1,A+1):
+		print (f"{xx*num} / {num} = {xx}")
+
+def tambah():
+	num = int(input(f"\n{blue}[{green}enter number{blue}]{green} >> "))
+	print ("")
+	for xx in range (1,A+1):
+		print (f"{xx} + {num} = {xx+num}")
+
+def kurang():
+	num = int(input(f"\n{blue}[{green}enter number{blue}]{green} >> "))
+	print ("")
+	for xx in range (1,A+1):
+		print (f"{xx} - {num} = {xx-num}")
+__________________________________________________________
+__________________________________________________________
+
+
+
+def replay():
+	ln = input(f"{cyan}Try again? [y/n]: ")
+	if ln=="n":exit()
+	elif ln=="y":menu()
+	else: print (f"{red}[!] Error!") ;sleep(1);replay()
+
+
+
+
+
+
+
+
+__________________________________________________________
+
+def add():
+	print ("\n")
+	print (48*"_")
+	print ("\nthis is IndoBlackDragon team\n")
+	haha = List("name",
+		message="add or like ",
+		choices=[
+			"Ahmad_adptr",
+			"Rifal_KY",
+			"Dharmadika_aga",
+			"Kuhaku",
+			"back"
+			]
+		)
+	www_google = prompt([haha])
+	if www_google["name"]=="Ahmad_adptr":
+		system("xdg-open https://www.facebook.com/profile.php?id=100071710504179")
+		menu()
+	elif www_google["name"]=="Rifal_KY":
+		system("xdg-open https://www.facebook.com/rifal.khusyaeri.5")
+		menu()
+	elif www_google["name"]=="Dharmadika_aga":
+		system("xdg-open https://www.facebook.com/profile.php?id=100074867864519")
+		menu()
+	elif www_google["name"]=="Kuhaku":
+		system("xdg-open https://www.facebook.com/profile.php?id=100074867864519")
+		menu()
+	elif www_google['name']=="back":
+		menu()
+
+
+def laporkan():
+	system("clear")
+	print ("tekan volume(+) + d untuk memilih\ntekan volume(+) + a untuk membatalkan\ntekan enter..\n")
+	hihi = Checkbox("BUG!",
+		message="your problem ",
+		choices=["script error","can't use any of the menu","out suddenly","contact a team member","dll"]
+		)
+	x=prompt([hihi])
+	try:
+		if x["BUG!"]==["script error"]:
+			print ("\nMaaf atas ketidaknyamanan Anda, script akan segera diperbaiki!")
+		if x["BUG!"]==["can't use any of the menu"]:
+			print ("\nSegera diperbaiki!")
+		if x["BUG!"]==["contact a team member"]:
+			system(f'''xdg-open https://wa.me/{ch}?text="maaf jika ada kesalahan! karna kita semua
+hanyalah manusia biasa
+
+*Jelaskan Masalah Anda!*"''') 
+		if x["BUG!"]==["dll"]:
+			print ("\nLaporan akan diproses");sleep(1)
+		if x["BUG!"]==[""]:
+			print ("\n[!] Jangan kosong");sleep(1);laporkan()
+		if x["BUG!"]==["out suddenly"]:
+			print ("\nTerima kasih sc akan segera diperbaiki!\n")
+
+	except TypeError: menu()
+__________________________________________________________
+
+
+
+
+
+
+
+__________________________________________________________
+__________________________________________________________
+# just bot
+
+
+__________________________________________________________
+var = {
+	"team":"IndoBlackDragon🇮🇩",
+	"github":"https://github.com/IndoBlackDragon",
+	"facebook":"@Ahmad_adptr",
+	"facebook2":"@Rifal KhusYaeri",
+	"facebook3":"@Dharmadhika Agga Sravana",
+
+		"profil":{
+
+			"name1":"Ahmad_adptr",
+
+				"name2":"Rifal",
+
+					"name3":"Aga"
+	}
+
+
+}
+dftr = [	'helo',			'limit',
+		'apa kabar',		'integral tentu',
+		'help',			'integral tak tentu',
+		'Assalamualaikum',	'diferensial'
+		'1 variabel',		'3 variabel',
+		'2 variabel',		'factor'
+	]
+__________________________________________________________
+
+from re import findall
+__________________________________________________________
+
+
+
+
+# claas bot
+
+class nonlinear(object):
+	def __init__(self,func):
+		self.func=func #name
+	def integral_tt(self):
+		solves = integrate(self.func,x)
+		return f"Answer  = {solves}"
+	def diferensials(self):
+		solves = diff(self.func,x)
+		return f"Answer  = {solves}"
+	def variabel1(self):
+		solves = solve(self.func,x)
+		return f"Answer  = {solves}"
+class linear(object):
+	def __init__(self,func,a,b):
+		self.func = func
+		self.x = a
+		self.y = b
+	def integral_t(self):
+		solves= integrate(self.func,(self.b,self.a))
+		return f"\nAnswer = {solves}"
+	def limits(self):
+		solves = limit(self.func,x,self.a)
+		return f"\nAnswer = {solves}"
+
+
+__________________________________________________________
+
+def math_bot():
+	name = input(f"{green}\nMasukkan nama Anda: ")
+	system("clear")
+	system("figlet -f future bot-math")
+	main(f"\nHello {green}{cyan}{name}{green} selamat datang di bot math\n")
+	main(f"ketik help untuk mendapatkan beberapa informasi\n")
+
+
+__________________________________________________________
+maths_bot,__________________________________________________________
+
+import re
+
+__________________________________________________________
+__________________________________________________________
+
+
+# bonus
+# menu
+
+plihan=[
+    "[A]. kali-kali",     "[B]. bagi-bagi",
+	"[C]. kurang-kurang", "[D]. tambah-tambah",
+	"[E]. Integral tentu","[E]. Integral tak tentu",
+	"[F]. limit",         "[G]. diferensial",
+	"[H]. factor",        "[I]. 1 variabel",
+	"[J]. 2 variabel",    "[K]. 3 variabel",
+	"[L]. back"
+	  ]
+
+# replay2()
+def replay2():
+	ln = input(f"{cyan}Try again? [y/n]: ")
+	if ln=="n":exit()
+	elif ln=="y":bonus()
+	else: print (f"{red}[!] Error!{normal}") ;sleep(1);replay2()
+
+def bonus():
+	print ("")
+	xx = [List("menu",message="pilihan",choices=plihan) ]
+	yy = prompt(xx)
+	if re.findall(r"kali",yy["menu"]):
+		kali()
+		print (f"\n{black}[[ program finished ]]{normal}")
+		replay2()
+	if re.findall(r"bagi",yy["menu"]):
+		bagi()
+		print (f"\n{black}[[ program finished ]]{normal}")
+		replay2()
+	if re.findall(r"kurang",yy["menu"]):
+		kurang()
+		print (f"\n{black}[[ program finished ]]{normal}")
+		replay2()
+	if re.findall(r"tambah",yy["menu"]):
+		tambah()
+		print (f"\n{black}[[ program finished ]]{normal}")
+		replay2()
+	if re.findall(r'back',yy["menu"]):
+		menu()
+	if re.findall(r"Integral tak",yy["menu"]):
+		solv = input("function: ∫ ")
+		ans = nonlinear(solv)
+		print (ans.integral_tt())
+		print (f"\n{black}[[ program finished ]]{normal}")
+		replay2()
+__________________________________________________________
+
+
+# function menu
+def menu():
+	system("clear")
+	print ("")
+	system("figlet -f pagga kalkulator | lolcat");print (green)
+#	print ("[•] 1. Penjumlahan")
+#	print ("[•] 2. pengurangan")
+#	print ("[•] 3. Pembagian")
+#	print ("[•] 4. Perkalian")
+#	print ("[•] 5. Bot math")
+#	print (f"{red}[•] 0. Exit")
+
+	try:
+		print ('\ntekan Ctrl+C untuk keluar secara cepat\ntekan volume(+) + s untuk ke bawah\ntekan volume(+) + w untuk ke atas\nlalu tekan enter..\n')
+		haha = List("ngajak_war?\n",message="choice ",choices=["Penjumlahan","Pengurangan","Pembagian","Perkalian","Bot math","Bonus","tambahkan pertemanan fb","laporkan masalah","Keluar"]) 
+		y = prompt([haha]) 
+		try:
+			tobat = y["ngajak_war?\n"] 
+		except TypeError:
+			print ("\n[[ program finished ]]\n");exit()
+#		expectation = int(input(f'\n{white}masukkan pilihan >>{blue} '))
+	except (NameError,ValueError,UnboundLocalError): 
+		main(f"{red}\n[!] Error!{green}\n");sleep(1);menu()
+
+
+	try:
+		if re.findall(r"Penj",tobat): # penjumlaham
+			penjumlahan();print (txt)
+			replay()
+		elif re.findall(r"Peng",tobat): # pengurangan
+			pengurangan();print (txt)
+			replay()
+		elif re.findall(r"Pemb",tobat): # pembagian
+			pembagian();print (txt)
+			replay()
+		elif re.findall(r"Perk",tobat): # perkalian
+			perkalian();print (txt)
+			replay()
+		elif re.findall(r"Kel",tobat): # exit
+			main(f"{blue}\n{bw}terima kasih telah gunakan script ini..{normal} \n")
+		elif re.findall(r"tambahk",tobat): # add
+			add()
+		elif re.findall(r"lapor",tobat): # bug
+			laporkan()
+		elif re.findall(r"Bo",tobat): # bug
+			bonus()
+	except (UnboundLocalError,ValueError):
+		main(f"\n{red}[!] Error\n")
+		menu()
+menu()
+
+
+__________________________________________________________
+__________________________________________________________
